0f2baf92707f9c7096045147a01f24ad8570e083
andre21cavalcante <andre.cavalcantejr21@gmail.com>
2022-03-14 11:10:16 -0300

    Ultilizando o chalk

---
+++ index.js
@@ -0,0 +1,10 @@
+import chalk from 'chalk';
+
+console.log(chalk.blue("boraaaaa"));
+
+const paragrafo = "Texto retornado por uma função "; 
+
+function texto(string){
+    return(string)
+}
+console.log(texto(paragrafo));
