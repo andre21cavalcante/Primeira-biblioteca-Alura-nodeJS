5e325e06656aa4fc0197949e84e702c85fc1b2a3
andre21cavalcante <andre.cavalcantejr21@gmail.com>
2022-03-14 11:45:08 -0300

    Tratamento de erro

--- index.js
+++ index.js
@@ -1,10 +1,17 @@
 import chalk from 'chalk';
 import fs from "fs";
 
+function trataErro(erro){
+    throw new Error(chalk.red(erro.code, "Não há arquivo no caminho"))
+}
+
 function pegarArquivo(caminhoDoArquivo){
     const encoding = "utf-8";
-    fs.readFile(caminhoDoArquivo, encoding, (_, texto)=> {
+    fs.readFile(caminhoDoArquivo, encoding, (erro, texto)=> {
+        if (erro) {
+            trataErro(erro)
+        }
         console.log(chalk.green(texto));
     })
 }
-pegarArquivo("./arquivos/texto1.md");
+pegarArquivo("./arquivos/");
