ed639118f5cdd6e0a37fa7ca3b1d2e36c0603565
andre21cavalcante <andre.cavalcantejr21@gmail.com>
2022-03-14 16:14:47 -0300

    fs promises

--- index.js
+++ index.js
@@ -7,11 +7,19 @@ function trataErro(erro){
 
 function pegarArquivo(caminhoDoArquivo){
     const encoding = "utf-8";
+    fs.promises.readFile(caminhoDoArquivo, encoding)
+    .then((texto) => (console.log(chalk.green(texto))))
+    .catch((erro) => trataErro(erro))
+}
+
+/*function pegarArquivo(caminhoDoArquivo){
+    const encoding = "utf-8";
     fs.readFile(caminhoDoArquivo, encoding, (erro, texto)=> {
         if (erro) {
             trataErro(erro)
         }
         console.log(chalk.green(texto));
     })
-}
-pegarArquivo("./arquivos/");
+}*/
+
+pegarArquivo("./arquivos/texto1.md");
